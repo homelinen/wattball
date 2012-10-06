c3b4e5dae527b812468370c0d8de45fe48099d42
Calum Gilchrist <calum@homelinen.org>
2012-10-06 19:24:43 +0100

    Initialised Repository

---
+++ README.md
@@ -0,0 +1,9 @@
+# Wattball #
+
+This is the code repository for the Wattball Project.
+
+## Authors ##
+
+## Installation ##
+
+## License ##
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wattball::Application.config.secret_token = 'ecee23384b91c47e6aa5b403d0b2aee722cbc04597bf1e230d2d3000f7e3fb7fec058ce06fc0e1dfa7bb5c5ebf57cfdf3fb717b011362dd14a0943d4ac67b683'
