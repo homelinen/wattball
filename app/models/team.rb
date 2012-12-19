bdded143e7e68efcd3d5345df04e4058b7966a9e
Calum Gilchrist <calum@homelinen.org>
2012-12-19 15:35:03 +0000

    Added Nested Attributes and made badge_file_name accessible

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,7 @@
 class Team < ActiveRecord::Base
   belongs_to :User
-  attr_accessible :badge, :teamName, :User_id
+  attr_accessible :badge, :badge_file_name, :teamName, :User, :User_id
   has_attached_file :badge, :styles => { :medium => "300x300>", :thumb => "100x100>"  }
+
+  accepts_nested_attributes_for :User
 end
