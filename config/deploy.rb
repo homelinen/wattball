6bd332eaa2c1e9a35901b6bb9204eda2aff4b766
Calum Gilchrist <calum@homelinen.org>
2013-01-03 18:24:12 +0000

    Wrote Capistrano Deployment Files

---
+++ config/deploy.rb
@@ -0,0 +1,34 @@
+require "bundler/capistrano"
+require "rvm/capistrano"
+
+set :application, "wattball"
+set :repository,  "git@github.com:homelinen/wattball.git"
+
+# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+server "eor.homelinen.org", :app, :web, :db, :primary => true
+
+set :deploy_to, "/srv/ruby/#{application}"
+
+ssh_options[:forward_agent] = true
+
+set :use_sudo, false
+
+set :rvm_ruby_string, "1.9.3@wattball"
+#before 'deploy:setup', 'rvm:export_gemset'
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
