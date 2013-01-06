792515f8ae05d3b9a93e8c4565cbb765953bb5d5
Calum Gilchrist <calum@homelinen.org>
2013-01-06 19:52:10 +0000

    Fixed a Syntax Error in the WattballMatch Model

--- app/models/wattball_match.rb
+++ app/models/wattball_match.rb
@@ -1,9 +1,8 @@
 class WattballMatch < ActiveRecord::Base
   belongs_to :event
-  has_many :playing_teams
 
-  belongs_to :team1, :class_name => :team
-  belongs_to :team2, :class_name => :team
+  belongs_to :team1, :class_name => Team
+  belongs_to :team2, :class_name => Team
 
   attr_accessible :team1, :team2
 end
