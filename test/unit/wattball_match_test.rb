fbeed623e9815aa3a6094fbf3577c23e3b454a3c
Calum Gilchrist <calum@homelinen.org>
2013-01-06 19:51:55 +0000

    Made A Rather Simple Test for WattballMatches

--- test/unit/wattball_match_test.rb
+++ test/unit/wattball_match_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
+require 'factory_girl'
 
 class WattballMatchTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  #
+  test "Succesfully Generate a Wattball Match" do
+
+    # Hacky way to ensure a tournament is present
+    if Tournament.first.nil?
+      FactoryGirl.create(:tournament)
+    end
+
+    # Generate 2 new teams
+    FactoryGirl.create_list(:team, 2)
+
+    @match = FactoryGirl.build(:wattball_match)
+    assert_not_nil(Team.find(@match.team1), "Has a tean")
+    assert_not_nil(Team.find(@match.team2), "Has a second team")
+
+    assert(@match.save!)
+  end
+
 end
