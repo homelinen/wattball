6c3d5f5216f2f5a8743f5f74867609e0c3bf6ee6
Calum Gilchrist <calum@homelinen.org>
2013-01-09 13:48:04 +0000

    Added a Helper for Concatenating the Names of a User
    Probably shouldn't be in application_helper, but it works.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  # Apparently bad practice to have this here, but whatever
+  def join_user_name(user)
+    concatName =  user.first_name.concat(" ").concat(user.last_name) unless user.nil?
+  end
 end
