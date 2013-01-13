6dfcf492248ad7052098ca4a0206794f7a793e9e
Calum Gilchrist <calum@homelinen.org>
2013-01-13 20:12:26 +0000

    Added Hurdle Match Relation to Event Model

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,8 @@ class Event < ActiveRecord::Base
   belongs_to :tournament
 
   has_one :wattball_match
+  has_one :hurdle_match
+  has_many :hurdle_times, :through => :hurdle_match
 
   attr_accessible :date, :end, :start, :status, :official_id, :tournament_id
 end
