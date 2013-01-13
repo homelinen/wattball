794f84137fb5d54360835260ef3735d27bc2802f
Calum Gilchrist <calum@homelinen.org>
2013-01-13 21:33:19 +0000

    Added a Helper to make a list of hurdlers and times

--- app/helpers/hurdle_matches_helper.rb
+++ app/helpers/hurdle_matches_helper.rb
@@ -1,2 +1,21 @@
 module HurdleMatchesHelper
+
+  # Print athletes and their times
+  def print_times(match)
+    outlist="<ul>"
+
+    match.hurdle_times.each do |times|
+      outlist += "\n<li>#{link_to join_user_name(times.athlete.user), times.athlete} - "
+      time = times.time
+      if time.nil?
+        timestring = "0:00"
+      else
+        timestring = "#{time}s"
+      end
+      outlist += timestring
+    end
+
+    outlist += "\n</ul>"
+    outlist.html_safe
+  end
 end
