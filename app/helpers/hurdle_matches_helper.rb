2e7ead22844acb52f9ecf30738440ca35d718f1e
Calum Gilchrist <calum@homelinen.org>
2013-01-13 17:44:29 +0000

    Created a time_to_mins_and_secs Method for Readability

--- app/helpers/hurdle_matches_helper.rb
+++ app/helpers/hurdle_matches_helper.rb
@@ -1,5 +1,12 @@
 module HurdleMatchesHelper
 
+  # Print a Time as 1:06s
+  #
+  # Useful for lengths of time, not dates
+  def time_to_mins_and_secs(time)
+        "#{time.min}:#{time.strftime("%S")}s"
+  end
+
   # Print athletes and their times
   def print_times(match)
     outlist="<ul>"
@@ -8,9 +15,10 @@ module HurdleMatchesHelper
       outlist += "\n<li>#{link_to join_user_name(times.athlete.user), times.athlete} - "
       time = times.time
       if time.nil?
-        timestring = "0:00"
+        # Could possibly get the athletes previous time
+        timestring = "No time"
       else
-        timestring = "#{time.min}:#{time.strftime("%S")}s"
+        timestring = time_to_mins_and_secs time
       end
       outlist += timestring
     end
