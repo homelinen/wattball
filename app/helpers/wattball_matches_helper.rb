838a7978797833f822eee157acd5577a4dcbea04
Calum Gilchrist <calum@homelinen.org>
2013-01-21 19:25:53 +0000

    Refactored Tournaments View to use a wattball_match_name helper

--- app/helpers/wattball_matches_helper.rb
+++ app/helpers/wattball_matches_helper.rb
@@ -1,2 +1,11 @@
 module WattballMatchesHelper
+
+  # Get a friendly string describing an event
+  def wattball_match_name(wattball_match)
+    %Q(
+      #{wattball_match.team1.teamName} vs #{wattball_match.team2.teamName}
+      at #{get_time_of_event(wattball_match.event.start)} 
+      on #{get_date_for_event wattball_match.event.date}
+    )
+  end
 end
