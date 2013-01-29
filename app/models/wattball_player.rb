153d418a1b8e983a67f4ae5328857bf9ca3fd661
Calum Gilchrist <calum@homelinen.org>
2013-01-29 10:23:50 +0000

    Added a Simple name Method to WattballPlayer Instances
    Concats the names of the Players user reference.

--- app/models/wattball_player.rb
+++ app/models/wattball_player.rb
@@ -1,3 +1,8 @@
 class WattballPlayer < Athlete
 
+  # Combine first and last names of user
+  # (This should be taken for all users)
+  def name
+    self.user.first_name + self.user.last_name
+  end
 end
