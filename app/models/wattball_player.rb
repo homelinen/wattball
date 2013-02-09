ceb4f830a5846451d7182e29ecb208fcaf18c08a
Calum Gilchrist <calum@homelinen.org>
2013-02-09 18:43:16 +0000

    Fixed WattballPlayer.name to Use a Space Between Names

--- app/models/wattball_player.rb
+++ app/models/wattball_player.rb
@@ -3,6 +3,6 @@ class WattballPlayer < Athlete
   # Combine first and last names of user
   # (This should be taken for all users)
   def name
-    self.user.first_name + self.user.last_name
+   [self.user.first_name, self.user.last_name].join " "
   end
 end
