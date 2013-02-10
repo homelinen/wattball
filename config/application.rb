aab60624a6927105f14f42d9f76f89aa644fe8bf
Calum Gilchrist <calum@homelinen.org>
2013-02-10 00:53:05 +0000

    Created a Simple all_models Method
    Had to add as a Global Helper Model and include in the application loader.

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Wattball
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Autoload files in lib
+    config.autoload_paths += %W(#{config.root}/lib/helpers)
   end
 end
