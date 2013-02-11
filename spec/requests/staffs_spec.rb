db95672de2af1612adf0f2193caa0c0dba76b4c8
Calum Gilchrist <calum@homelinen.org>
2013-02-11 02:15:38 +0000

    Added a Simple Request Test For Making a Staffer

--- spec/requests/staffs_spec.rb
+++ spec/requests/staffs_spec.rb
@@ -1,11 +1,34 @@
 require 'spec_helper'
+require "faker"
 
 describe "Staffs" do
   describe "GET /staffs" do
     it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       get staffs_path
       response.status.should be(200)
+    end
+  end
+
+  describe "Create a new staff member", :type => :feature do
+    it "Click New Staff Button" do
+      visit staffs_path
+
+      click_link 'New Staff'
+
+      current_path.should == new_staff_path
+    end
+
+    it "Fill New Staff Form" do
+      visit new_staff_path
+
+      within("#new_staff") do
+        # Create a new User
+        # Create a new Address
+        fill_in 'staff_telephone', :with => Faker::PhoneNumber.phone_number
+        click_button 'Create Staff'
+      end
+
+      # Check Staff Count has increased
     end
   end
 end
