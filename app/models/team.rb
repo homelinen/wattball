24b42ef273ad429237872da29f617d39b22e43e0
Calum Gilchrist <calum@homelinen.org>
2013-03-04 22:52:05 +0000

    Added More Validation to Teams and Fixed Assocs
    The wattball_matches association was inherently broken, now works.

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,13 +2,20 @@ class Team < ActiveRecord::Base
   belongs_to :user
   belongs_to :tournament
 
-  has_many :wattball_player
-  has_many :wattball_matches
+  has_many :wattball_players
+  has_many :wattball_matches_as_team1, :class_name => 'WattballMatch', :foreign_key => :team1_id
+  has_many :wattball_matches_as_team2, :class_name => 'WattballMatch', :foreign_key => :team2_id
 
   attr_accessible :badge, :badge_file_name, :teamName, :user, :user_id, :tournament_id
   has_attached_file :badge, :styles => { :medium => "300x300>", :thumb => "100x100>"  }
 
   accepts_nested_attributes_for :user
 
-  validates_associated :tournament
+  validates_presence_of :user, :teamName, :tournament
+  validates_associated :tournament, :user
+
+  # Get all the matches team is playing in
+  def wattball_matches
+    self.wattball_matches_as_team1 + self.wattball_matches_as_team2
+  end
 end
