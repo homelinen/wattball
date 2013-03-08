644227f9df2cedd0841ec5bd2df38595b8577b9f
Calum Gilchrist <calum@homelinen.org>
2013-03-08 13:45:29 +0000

    Refactored Scores to Use WattballMatch instead of Event
    Could have made scores more generic, but not worth it.

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,11 +1,11 @@
 class Score < ActiveRecord::Base
   belongs_to :wattball_player
-  belongs_to :event
+  belongs_to :wattball_match
 
   # Number of goals prevents duplicates in the table
-  attr_accessible :amount
+  attr_accessible :amount, :wattball_match, :wattball_player
 
-  validates_presence_of :event, :wattball_player
+  validates_presence_of :wattball_match, :wattball_player
 
   validate :amount, :presence => true, :numericality => { :greater_than => 0 }
 
@@ -13,14 +13,16 @@ class Score < ActiveRecord::Base
   #
   # match - An WattballMatch with two different teams
   #
+  # FIXME: Should this be static?
+  #
   # Example
   #   Score.match_score(WattballMatch.first) 
   #   => [3, 0]
   #
   # Returns an array of scores [team1_score, team2_score]
   def self.match_score(match)
-    score1 = calculate_score(match.team1, match.event) 
-    score2 = calculate_score(match.team2, match.event) 
+    score1 = calculate_score(match.team1, match) 
+    score2 = calculate_score(match.team2, match) 
 
     [ score1, score2 ]
   end
@@ -31,8 +33,8 @@ class Score < ActiveRecord::Base
   # event - The event where the team played a match
   #
   # Returns the points scored by a team in the match
-  def self.calculate_score(team, event)
-    Score.includes(:wattball_player).includes(:event).where(:wattball_players => {"team_id" => team.id}, :events => { :id => event.id }).sum(:amount)
+  def self.calculate_score(team, match)
+     match.scores.includes(:wattball_player).where(:wattball_players => {:team_id => team}).sum(:amount) 
   end
 
 end
--- app/models/wattball_match.rb
+++ app/models/wattball_match.rb
@@ -8,4 +8,9 @@ class WattballMatch < ActiveRecord::Base
   attr_accessible :team1, :team2
 
   validates_presence_of :team1, :team2, :event
+
+  def self.details(lim)
+    lim = nil if lim == 0
+    WattballMatch.joins(:event).order(:date).limit(lim)
+  end
 end
