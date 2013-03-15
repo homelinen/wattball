91fb24b69448054a12e9d64dac11a03925707c81
Calum Gilchrist <calum@homelinen.org>
2013-03-15 14:34:30 +0000

    Fixed Redirects for User Sign Ins
    If not signed in, user is prompted to sign in and then returned to
    the page they were on previosuly.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,26 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   before_filter :instantiate_controller_and_action_names
+  before_filter :store_location
 
   rescue_from CanCan::AccessDenied do |exception|
-    flash[:alert] = "Access denied."
-    redirect_to root_url
+    flash[:alert] = "Sorry, you need to login or register to do that."
+    redirect_to new_user_session_path
   end
    
   def instantiate_controller_and_action_names
       @current_action = action_name
       @current_controller = controller_name
+  end
+
+
+  def store_location
+    # store last url as long as it isn't a /users path
+    session[:previous_url] = request.fullpath unless request.fullpath =~ /\/users/
+  end
+
+  def after_sign_in_path_for(resource)
+    session[:previous_url] || root_path
   end
 
   add_breadcrumb "home", :root_path
