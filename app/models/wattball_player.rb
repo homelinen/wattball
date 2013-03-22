905b1df3754eeb388e8b1820575f75a323a788e4
Calum Gilchrist <calum@homelinen.org>
2013-03-22 02:58:17 +0000

    Changed the Wattball Player Creation Page to Be Far Easier to Use

--- app/models/wattball_player.rb
+++ app/models/wattball_player.rb
@@ -8,8 +8,8 @@ class WattballPlayer < ActiveRecord::Base
   # organistationTag means an ID, avoiding naming errors here
   attr_accessible :dob, :phone_number, :team_id, :team, :user, :contact, :user_attributes, :contact_attributes
 
-  accepts_nested_attributes_for :user, :contact
+  accepts_nested_attributes_for :user, :contact, :team
 
   validates_associated :user, :contact
-  validates_presence_of :user, :dob, :team, :phone_number
+  validates_presence_of :user, :dob, :team_id, :phone_number
 end
