4bd2fabc7b780c81d4b0fedc84f2df2cd24526a7
Calum Gilchrist <calum@homelinen.org>
2013-03-24 20:20:49 +0000

    Added Sign Up Links to the Tournaments drop down

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -4,10 +4,19 @@ class Tournament < ActiveRecord::Base
 
   has_many :events
   has_many :teams
+  has_many :hurdle_players
 
   attr_accessible :endDate, :max_competitors, :name, :startDate, :sport_id, :competition_id
 
   validates_presence_of :name, :startDate, :endDate, :max_competitors
 
   validates :max_competitors, :numericality => { :greater_than => 0 }
+
+  def competitors 
+    
+    count = teams.count unless teams.empty?
+    count = hurdle_players.count unless hurdle_players.empty?
+
+    count
+  end
 end
